68518e633b9eda69718943c25f4a48d5a05b213a
hideckies <hideckies@gmail.com>
2022-07-27 13:32:16 +0900

    added vite.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,15 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			define: {
-				'process.env': process.env
-			}
-		}
+		adapter: adapter()
 	}
 };
 
